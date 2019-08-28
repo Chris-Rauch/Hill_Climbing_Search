2c0d16e8348d9fba1f31584b3361567bca2b9cb6
Chris-Rauch <rauch.christopher13@gmail.com>
2019-08-27 17:45:41 -0700

    finished program outline. Needs debugging

--- main.cpp
+++ main.cpp
@@ -1,11 +1,213 @@
 //@main.cpp
 
+#ifndef ASTAR
+#define ASTAR
+
 #include <iostream>
+#include <cstdlib>
+#include <queue>
+#include <vector>
+#include <list>
+#include <cmath>
+#include <iterator>
 #include "node.h"
 using namespace std;
 
+//FUNCTION HEADERS
+void randomize(node& board);
+void display(const node& board);
+bool isGoalState(const node& board);
+void calcHeuristic(node& board);
+vector<node> getChildren(node& board);
+
+
 int main() {
-  cout << "Hello World!\n";
-  
-  return 1;
+    priority_queue<node, std::vector<node>, CompareHeuristic> frontier;
+    list<node> visited;
+    node board;
+
+    randomize(board);
+    frontier.push(board);
+
+    //A*
+    while(true) {
+        //pop node off frontier
+        node temp(frontier.top());
+        frontier.pop();
+
+        //check goal state
+        if(isGoalState(temp)) {
+            display(temp);
+            break;
+        }
+        
+        //check if already visited
+        bool wasVisited = false;
+        list<node>::iterator it;
+        for(it = visited.begin(); it != visited.end(); ++it) {
+            if(*it == temp) {
+                wasVisited = true;
+                break;
+            } 
+        }
+
+        if(wasVisited == false) {
+            //add to visited
+            visited.push_back(temp);
+            
+            //add children to frontier
+            vector<node> children;
+            children = getChildren(temp);
+            for(short x = 0; x < children.size(); ++x) {
+                frontier.push(children[x]);
+            }
+        }
+    }
+
+    return 1;
 }
+
+//==== GET_CHILDREN ====
+vector<node> getChildren(node& board) {
+    int size = sqrt(SIZE);
+    vector<node> children;
+    
+    //check up
+    if(board.currPos / size != 0) {
+        //create a new node and copy current board state
+        node child(board);
+        
+        //swap the 0 tile with the above tile
+        child.value[child.currPos] = board.value[board.currPos - size];
+        child.value[child.currPos - size] = board.value[board.currPos];
+        child.currPos = board.currPos - size;
+        
+        //calc new heuristic
+        calcHeuristic(child);
+        
+        //add to children vector
+        children.push_back(child);
+    }
+  
+    //check down
+    if(board.currPos / size != (size-1)) {
+        //create a new node and copy current board state
+        node child(board);
+        
+        //swap the 0 tile with the above tile
+        child.value[child.currPos] = board.value[board.currPos + size];
+        child.value[child.currPos + size] = board.value[board.currPos];
+        child.currPos = board.currPos + size;
+        
+        //calc new heuristic
+        calcHeuristic(child);
+        
+        //add to children vector
+        children.push_back(child);
+    }
+
+    //check left
+    if(board.currPos % size != 0) {
+        //create a new node and copy current board state
+        node child(board);
+        
+        //swap the 0 tile with the above tile
+        child.value[child.currPos] = board.value[board.currPos - 1];
+        child.value[child.currPos - 1] = board.value[board.currPos];
+        child.currPos = board.currPos - 1;
+        
+        //calc new heuristic
+        calcHeuristic(child);
+        
+        //add to children vector
+        children.push_back(child);
+    }
+
+    //check right
+    if(board.currPos % size != (size-1)) {
+        //create a new node and copy current board state
+        node child(board);
+        
+        //swap the 0 tile with the above tile
+        child.value[child.currPos] = board.value[board.currPos + 1];
+        child.value[child.currPos + 1] = board.value[board.currPos];
+        child.currPos = board.currPos + 1;
+        
+        //calc new heuristic
+        calcHeuristic(child);
+        
+        //add to children vector
+        children.push_back(child);
+    }
+
+    return children;
+}
+
+//==== CALC_HEURISTIC ====
+void calcHeuristic(node& board) {
+    int size = sqrt(SIZE);
+    int heuristic = 0;
+    for(int x = 0; x < SIZE; ++x) {
+        //get the current tile value
+        int tile = board.value[x];
+
+        //find the row & column it's SUPPOSED to be in
+        int goalRow = tile / size;
+        int goalColumn = tile % size;
+
+        //find the row & column it's CURRENTLY in
+        int currRow = x / size;
+        int currColumn = x % size;
+
+        //add there differences
+        heuristic =+ abs(goalRow-currRow) + abs(goalColumn-currColumn);
+    }
+    board.heuristic = heuristic;
+}
+
+//==== IS_GOAL_STATE ====
+bool isGoalState(const node& board) {
+    bool goalState = true;
+
+    for(short x = 0; x < SIZE; ++x) {
+        if(board.value[x] != x) {
+            goalState = false;
+            break;
+        }
+    }
+    return goalState;
+}
+
+//==== RANDOMIZE ====
+void randomize(node& board) {
+    bool number[SIZE] = {false};
+    
+    for(short x = 0; x < SIZE; ++x) {
+        int y = rand() % SIZE; 
+        if(number[y] == false) { 
+            board.value[x] = y;
+            number[y] = true;
+
+            if(y == 0) {
+                board.currPos = x; //sets the index of the 0 tile    
+            }
+        }
+        else {
+            --x;   
+        }
+    }
+}
+
+//==== DISPLAY ====
+void display(const node& board) {
+    for(short x = 0; x < SIZE; ++x) {
+        if(x % 3 == 0) {
+            cout << endl;
+        }
+        cout << board.value[x] << " ";
+    }
+    cout << "Current Position: " << board.currPos << endl;
+}
+
+
+#endif
--- node.h
+++ node.h
@@ -1,2 +1,36 @@
 //@node.h
 #include "node.cpp"
+const int SIZE = 9;
+
+struct node {
+    node(){}
+    node(const node& rhs);
+    
+    int value[SIZE];
+    int heuristic;
+    int currPos; //represents the index of the empty tile, aka 0
+};
+
+struct CompareHeuristic {
+    bool operator()(node const& lhs, node const& rhs) {
+        return lhs.heuristic > rhs.heuristic;
+    }
+};
+
+node::node(const node& rhs) : heuristic(rhs.heuristic), currPos(rhs.heuristic) {
+    for(short x = 0; x < SIZE; ++x) {
+        this->value[x] = rhs.value[x];
+    }
+}
+
+inline bool operator==(const node& lhs, const node& rhs) {
+    bool visited = true;
+
+    for(short x = 0; x < SIZE; ++x) {
+        if(lhs.value[x] != rhs.value[x]) {
+            visited = false;
+            break;
+        }
+    }
+    return visited;
+}
